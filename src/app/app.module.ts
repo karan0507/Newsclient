36db64b19e7b08bafb142cf432ec256112056b85
Yash <yash.talegaonkar37@gmail.com>
2020-06-22 15:56:20 +0530

    initalized
    Signed-off-by: Yash <yash.talegaonkar37@gmail.com>

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './admin_panel/login/login.component';
+import { PostComponent } from './admin_panel/post/post.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    PostComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
